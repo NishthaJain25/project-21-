42c26b708c5e9b07d2437c0c97e2c9d540ddf2f0
NishthaJain25 <80957287+NishthaJain25@users.noreply.github.com>
2021-03-27 22:52:44 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,82 @@
+var canvas;
+var block1,block2,block3,block4;
+var ball, edges;
+var music;
+
+function preload(){
+    // load sound here
+}
+
+
+function setup(){
+    canvas = createCanvas(800,600);
+
+    block1 = createSprite(30,600,200,30);
+    block1.shapeColor = "blue";
+
+    block2 = createSprite(245,600,200,30);
+    block2.shapeColor = "orange";
+
+    //create two more blocks i.e. block3 and block4 here
+    
+    block3 = createSprite(460,600,200,30);
+    block3.shapeColor  = "red";
+    
+    block4 = createSprite(700,600,200,30);
+    block4.shapeColor = "green";
+
+    ball = createSprite(random(20,750),100, 40,40);
+    ball.shapeColor = rgb(255,255,255);
+
+    //write code to add velocityX and velocityY
+    ball.velocityX = 4;
+    ball.velocityY = 4;
+
+}
+
+function draw() {
+    background(rgb(169,169,169));
+
+    edges=createEdgeSprites();
+    ball.bounceOff(edges);
+
+    
+    //write code to bounce off ball from the block1 
+    if(block1.isTouching(ball) && ball.bounceOff(block1)){
+        ball.shapeColor = "blue";
+        music.play();
+        ball.velocityX = 0;
+        ball.velocityY = 0;
+
+        
+
+    }
+
+    if(block2.isTouching(ball)){
+        ball.shapeColor = "orange";
+        //write code to set velocityX and velocityY of ball as 0
+        ball.velocityX = 0;
+        ball.velocityY = 0;
+
+        //write code to stop music
+
+    }
+
+    //write code to bounce off ball from the block3
+    if(block3.isTouching(ball)){
+        ball.shapeColor = "red";
+        ball.velocityX = 0;
+        ball.velocityY = 0;
+
+    }
+
+
+    //write code to bounce off ball from the block4
+    if(block4.isTouching(ball)){
+        ball.shapeColor = "green";
+        ball.velocityX = 0;
+        ball.velocityY = 0;
+    }
+
+    drawSprites();
+}
